34bc994174b0b5eb0f38bdeabd01c00d3e6d49c8
Mudit Choudhary <cmudit60@gmail.com>
2022-02-07 19:03:07 +0530

    Add the hoverEvent which will change grid color on hover

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ function createGrid(size) {
         const row = document.createElement('div');
         row.classList.add('row');
         for (let j = 0; j < size; j++) {
-            const div = document.createElement('div');
-            div.classList.add('column');
-            row.appendChild(div);
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            row.appendChild(grid);
         }
         container.appendChild(row);
     }
 }
+
+function changeGridColor() {
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.addEventListener('mouseover', () => {
+       grid.style.backgroundColor = 'black';
+    }));
+}
 createGrid(16);
+changeGridColor();
