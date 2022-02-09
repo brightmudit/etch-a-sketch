3c9676dfa830bb202ddea7bcf8c0a3acb273f37d
Mudit Choudhary <cmudit60@gmail.com>
2022-02-09 22:10:51 +0530

    Add function to generate random colors

--- script.js
+++ script.js
@@ -13,40 +13,42 @@ function createGrid(size) {
 }
 
 function changeGridColor(e) {
-    if(e.buttons === 1) e.target.style.backgroundColor = 'black';
+    if(e.buttons === 1) e.target.style.backgroundColor = generateRandomColor();
 }
 
-function removeOldGrid() {
-    const rows = document.querySelectorAll('.row');
-    const size = rows.length;
-    for (let i = 0; i < size; i++) {
-        const row = document.querySelector('.row');
-        for (let j = 0; j < size; j++) {
-            const grid = document.querySelector('.grid');
-            row.removeChild(grid);
-        }
-        container.removeChild(row);
+function generateRandomColor() {
+    const LETTERS = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += LETTERS[Math.floor((Math.random() * LETTERS.length))];
+    }
+    return color;
+}
+
+function removeOldGrid(currentSize) {
+    for (let i = 0; i < currentSize; i++) {
+        container.removeChild(document.querySelector('.row'));
     }
 }
 
 function askUserSize() {
-    const size = parseInt(prompt('Please enter number of squares per side for the new grid', '16'));
-    return size;
+    currentSize = parseInt(prompt('Please enter number of squares per side for the new grid', '16'));
+    return currentSize;
 }
 
-function createNewGrid() {
-    removeOldGrid();
+function createNewGrid(currentSize) {
+    removeOldGrid(currentSize);
     createGrid(askUserSize());
     
 }
 
 
+// Important Variables
+const DEFAULT_SIZE = 16;
+let currentSize = DEFAULT_SIZE;
 const container = document.querySelector('.container');
 const btn = document.querySelector('.grid-changer');
-let mouseDown = false;
-document.body.addEventListener('mousedown', () => mouseDown = true);
-document.body.addEventListener('mouseup', () => mouseDown = false);
 
-btn.addEventListener('click', createNewGrid);
+btn.addEventListener('click', () => createNewGrid(currentSize));
 
-createGrid(16);
+createGrid(currentSize);
